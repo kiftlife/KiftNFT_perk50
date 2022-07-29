56ee7b3e46580293ffbb82bb658dcaa80da959ce
Sante Kotturi <sakotturi@gmail.com>
2022-07-28 18:51:37 -0700

    cleaned up logs

--- contracts/01_BasicApproach.sol
+++ contracts/01_BasicApproach.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.9;
 
 import "./Kiftables/Kiftables.sol";
-import "hardhat/console.sol";           // for debuggings
+// import "hardhat/console.sol";           // for debuggings
 
 contract KiftablesPerkFiftyOff {
     Kiftables public kiftables;
@@ -14,17 +14,7 @@ contract KiftablesPerkFiftyOff {
     }
 
     function redeemPerk(uint256 tokenId) public {
-        // debug
-        console.log("Redeeom contract deployed at %s", address(this));
-        console.log("Redeeming perk for tokenId: %s by %s", tokenId, msg.sender );
-        uint256 balance = kiftables.balanceOf(msg.sender);
-        console.log("Balance: %s", balance);
-        bool isActive = kiftables.isPublicSaleActive();
-        console.log("Kiftables sale active: %s", isActive  );
-        // end debug
-
         address tokenOwner = kiftables.ownerOf(tokenId);
-        console.log("Token owner: %s", tokenOwner);
         require(
             tokenOwner == msg.sender,
             "Cannot redeem perk for token you dont own"
